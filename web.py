4fead8a17c1321e14225e940bf09d5d7b536bc7b
anhtv <anhtv26062000@gmail.com>
2021-06-24 20:58:23 +0700

    check CCCD/CMND

--- web.py
+++ web.py
@@ -1,4 +1,3 @@
-from os import add_dll_directory
 from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -28,35 +27,38 @@ def load_image_into_numpy_array(data):
 
 def sort_result(result):
     sorted_result = {}
-    id = result['id'] + ' -- confidence: '  + str(result['id_conf'])
-    sorted_result['ID'] = id
+    id = result["id"] + " -- confidence: " + str(result["id_conf"])
+    sorted_result["ID"] = id
 
-    name = result['name'] + ' -- confidence: ' + str(result['name_conf'])
-    sorted_result['Name'] = name
+    name = result["name"] + " -- confidence: " + str(result["name_conf"])
+    sorted_result["Name"] = name
 
-    birthday = result['birthday'] + ' -- confidence: ' + str(result['birthday_conf'])
-    sorted_result['Birthday'] = birthday
+    birthday = result["birthday"] + " -- confidence: " + str(result["birthday_conf"])
+    sorted_result["Birthday"] = birthday
 
-    address = result['address'] + ' -- confidence: ' + str(result['address_conf'])
-    sorted_result['Address'] = address
+    address = result["address"] + " -- confidence: " + str(result["address_conf"])
+    sorted_result["Address"] = address
 
-    hometown = result['hometown'] + ' -- confidence: ' + str(result['hometown_conf'])
-    sorted_result['Hometown'] = hometown
-    
+    hometown = result["hometown"] + " -- confidence: " + str(result["hometown_conf"])
+    sorted_result["Hometown"] = hometown
 
-    document = result['document']
+    document = result["document"]
 
-    if document == 'CCCD':
+    if document == "CCCD":
 
-        national = result['national'] + ' -- confidence: ' + str(result['national_conf'])
-        sorted_result['National'] = national
-        
-        ethnicity = result['ethnicity'] + ' -- confidence: ' + str(result['ethnicity_conf'])
-        sorted_result['Ethnicity'] = ethnicity
+        national = (
+            result["national"] + " -- confidence: " + str(result["national_conf"])
+        )
+        sorted_result["National"] = national
 
-        sorted_result['Document'] = result['document']
-    elif document == 'CMND':
-        sorted_result['Document'] = result['document']
+        ethnicity = (
+            result["ethnicity"] + " -- confidence: " + str(result["ethnicity_conf"])
+        )
+        sorted_result["Ethnicity"] = ethnicity
+
+        sorted_result["Document"] = result["document"]
+    elif document == "CMND":
+        sorted_result["Document"] = result["document"]
     return sorted_result
 
 
@@ -67,7 +69,7 @@ async def index(request: Request):
 
 
 # get result page
-@app.post("/result")
+@app.post("/result", response_class=HTMLResponse)
 async def upload_image(request: Request, image: UploadFile = File(...)):
     with open("./static/images/destination.jpg", "wb") as buffer:
         shutil.copyfileobj(image.file, buffer)
@@ -76,12 +78,11 @@ async def upload_image(request: Request, image: UploadFile = File(...)):
         data = base64.b64encode(img_file.read()).decode("utf-8")
 
     send_data = {"image": data}
-    response = requests.post(url, data=json.dumps(send_data))
+    response = requests.post(url, json=send_data)
     result = response.json()
-    # result = {
-    #     'address': 'Bến Cả, Yên Lập, Vĩnh Tường Vĩnh Phúc', 'address_conf': [0.9249022086461385, 0.931002163887024], 'expiry': '2708/202012', 'expiry_conf': 0.8200896314599297, 'birthday': '15/10-1998', 'birthday_conf': 0.9085860788822174, 'sex': 'Nữ', 'sex_conf': 0.7451381385326385, 'hometown': 'Yên Lập, Vĩnh Tường, Vĩnh Phúc', 'hometown_conf': [0.9324268528393337, 0.9337636345908755], 'id': '026198001937', 'id_conf': 0.9309668292601904, 'name': 'NGUYỄN THỊ QUỲNH', 'name_conf': 0.9327027276158333, 'ethnicity': 'Kinh', 'ethnicity_conf': 'N/A', 'national': 'N/A', 'national_conf': 'N/A', 'document': 'CCCD', 'detect_runtime': 0.763115406036377, 'read_runtime': 1.9872732162475586
-    # }
-    result = sort_result(result)
+    if "detail" not in result.keys():
+        result = sort_result(result)
+
     return templates.TemplateResponse(
         "result.html", {"request": request, "result": result}
     )
@@ -98,11 +99,13 @@ async def test_image_1(request: Request):
         data = base64.b64encode(img_file.read()).decode("utf-8")
 
     send_data = {"image": data}
-    response = requests.post(url, data=json.dumps(send_data))
-    print(type(response.json()))
+    response = requests.post(url, json=send_data)
+    result = response.json()
+    if "detail" not in result.keys():
+        result = sort_result(result)
 
     return templates.TemplateResponse(
-        "result.html", {"request": request, "result": response.json()}
+        "result.html", {"request": request, "result": result}
     )
 
 
@@ -117,10 +120,13 @@ async def test_image_2(request: Request):
         data = base64.b64encode(img_file.read()).decode("utf-8")
 
     send_data = {"image": data}
-    response = requests.post(url, data=json.dumps(send_data))
+    response = requests.post(url, json=send_data)
+    result = response.json()
+    if "detail" not in result.keys():
+        result = sort_result(result)
 
     return templates.TemplateResponse(
-        "result.html", {"request": request, "result": response.json()}
+        "result.html", {"request": request, "result": result}
     )
 
 
@@ -135,8 +141,11 @@ async def test_image_3(request: Request):
         data = base64.b64encode(img_file.read()).decode("utf-8")
 
     send_data = {"image": data}
-    response = requests.post(url, data=json.dumps(send_data))
+    response = requests.post(url, json=send_data)
+    result = response.json()
+    if "detail" not in result.keys():
+        result = sort_result(result)
 
     return templates.TemplateResponse(
-        "result.html", {"request": request, "result": response.json()}
+        "result.html", {"request": request, "result": result}
     )
